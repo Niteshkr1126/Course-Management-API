59585e3657d9882b0bbbd20578effed7d9a77918
Nitesh Kumar <niteshkr1126@gmail.com>
2018-03-25 17:24:38 +0530

    Commit

---
+++ Course Management API/src/main/java/com/kumar/nitesh/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.kumar.nitesh.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	
+	
+
+}
